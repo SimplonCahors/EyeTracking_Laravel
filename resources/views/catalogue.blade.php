3d128ed41a1dc35af0e10f41087c114bfc994b2e
Fowder <emile.troccaz@hotmail.com>
2018-03-19 16:20:51 +0100

    Ajout route catalogue, page catalogue et css catalogue

---
+++ resources/views/catalogue.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Catalogue</title>
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="css/catalogue.css">
+</head>
+<body>
+    <section class="container">
+        <article class="comics">
+            <div class="infos">
+                <img src="/bd.jpg" alt="cover">
+                <ul>
+                    <li>Tintin au Pays des kangourous</li>
+                    <li>Auteur</li>
+                    <li>Edition</li>
+                </ul>
+                </div>
+            <button>Lire</button>
+        </article>
+        <article class="comics">
+        <div class="infos">
+            <img src="/bd.jpg" alt="cover">
+            <ul>
+                <li>Titre</li>
+                <li>Auteur</li>
+                <li>Edition</li>
+            </ul>
+            </div>
+            <button>Lire</button>
+        </article>
+        <article class="comics">
+        <div class="infos">
+            <img src="/bd.jpg" alt="cover">
+            <ul>
+                <li>Titre</li>
+                <li>Auteur</li>
+                <li>Edition</li>
+            </ul>
+            </div>
+            <button>Lire</button>
+        </article>
+        <article class="comics">
+        <div class="infos">
+            <img src="/bd.jpg" alt="cover">
+            <ul>
+                <li>Titre</li>
+                <li>Auteur</li>
+                <li>Edition</li>
+            </ul>
+            </div>
+            <button>Lire</button>
+        </article>
+    </section>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/catalogue', function () {
+    return view('catalogue');
+});
