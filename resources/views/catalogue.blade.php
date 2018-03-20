8cb4513e2ece26e900bc7616e4207fb437df4598
Fowder <emile.troccaz@hotmail.com>
2018-03-20 15:29:49 +0100

    Merge

--- resources/views/catalogue.blade.php
+++ resources/views/catalogue.blade.php
@@ -6,26 +6,40 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Catalogue</title>
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
     <link href="css/app.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-        @include('layout.navbar')
-    <section class="containers">
+@include('layout.navbar')
+
+    <section class="containers_catalog">
+            <!-- ICI LE IF POUR L'ADMIN + BOUTON AJOUTER -->        
+            <a id="ajouter_catalog" href=""><i class="material-icons catalogue">add_circle_outline</i></a>
+        <!-- REQUÊTE POUR AFFICHER LES BD DÉJÀ LUES CLASSÉES PAR ORDRE CROISSANT -->
+
+
     @foreach ($comics as $comic)
-
-        <article class="comics">
-        <div class="infos">
-            <img src="/bd.jpg" alt="cover">
-            <ul>
-                <li>{{$comic->com_title}}</li>
-                <li>{{$comic->com_author}}</li>
-                <li>{{$comic->com_publisher}}</li> 
-                <li>{{$comic->com_timestamp}}</li>             
-            </ul>
-
+        <article class="comics_catalog">
+            
+                <img class="img_catalog" src="/bd.jpg" alt="cover">
+                <div class="infos_catalog">
+                <ul>
+                    <li>{{$comic->com_title}}</li>
+                    <li>{{$comic->com_author}}</li>
+                    <li>{{$comic->com_publisher}}</li> 
+                    <li>{{$comic->com_timestamp}}</li>             
+                </ul>
+            
+         
+            <div class="read_edit_catalog">
+                
+                <!-- ICI LE IF POUR L'ADMIN + BOUTON MODIFIER -->
+                <!-- <a><button class="buttons" id="button_edit_catalog">Modifier</button></a> -->
+                <a><button class="buttons">Lire</button></a>
             </div>
-            <button class="buttons">Lire</button>
-        </article>
+            </div>
+    </article>
     @endforeach
 </section>  
 <div class="nav_catalog">
