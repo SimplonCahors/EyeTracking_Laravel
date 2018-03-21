3adde5c1e53818a3e5d0f7f31b564853ecb3d7a7
echayotte <echayotte@gmail.com>
2018-03-21 13:47:34 +0100

    creation view showPage

---
+++ resources/views/showPage.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <p>page lisible n° : </p>
+    
+    <img src="storage/{{ $showPage }}">
+{{--      
+    @php
+    foreach ($pages as $pages) {
+        @endphp
+            <img src="storage/{{ $pages->pag_image }}">
+        @php
+    }
+    @endphp  --}}
+</body>
+</html>
+
+
+{{--  /comics2/page1  --}}
+
+
+
