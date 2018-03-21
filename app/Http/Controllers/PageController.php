2ab25eca5ba2177846d27a3ed8c2f6d257175192
echayotte <echayotte@gmail.com>
2018-03-21 16:22:13 +0100

    function show fonctionne

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,27 +6,35 @@ use Illuminate\Http\Request;
 
 use DB;
 
+function screen($param)
+{
+    print "<pre>";
+    print_r($param);
+    print "</pre>";
+}
+
 class PageController extends Controller
 {
     public function create($idBD)
     {
         $numeroPage = $_POST['numeroPage'];
         $image = $_POST['image'];
-            
-            
+        
+        
         DB::table('pages')->insert(
-                array('pag_image' => $image,
-                      'pag_number' => $numeroPage,
-                      'fk_com_oid' => $idBD)
-            );
-       
+            array('pag_image' => $image,
+            'pag_number' => $numeroPage,
+            'fk_com_oid' => $idBD)
+        );
+        
         return redirect()->back()->with('message', 'Page ajoutée dans la base de données.');
     }
 
-    public function show()
+    public function show($idPage)
     {
-        $pages = DB::table('pages')->get();
-        // $pages = DB::table('pages')->get();
-        // return view('showPage', ['pages' => $pages]);
+        $board = DB::table('pages')->where('pag_number', $idPage)->pluck('pag_image');
+
+        //  screen($board[0]);
+        return view('showPage', ['page' => $board[0]]);
     }
 }
