b25dc488d701ceafb1d1d4b372db77a4ff9c6058
BMellet <benzero.m@gmail.com>
2018-03-21 11:25:57 +0100

    smoother scroll

--- resources/assets/js/board.js
+++ resources/assets/js/board.js
@@ -2,11 +2,21 @@
 var screen;
 document.getElementById("scrollUpBoard").addEventListener("mouseover", function(){
 	screen = $(window).height();
-	window.scrollBy(0, -screen);
+	window.scrollBy(
+		{
+			top:-screen,
+			left:0,
+			behavior:'smooth' 
+		});
 });
 document.getElementById("scrollDownBoard").addEventListener("mouseover", function(){
 	screen = $(window).height();
-	window.scrollBy(0, screen);
+	window.scrollBy(
+		{
+			top:screen,
+			left:0,
+			behavior:'smooth' 
+		});
 
 });		
 
