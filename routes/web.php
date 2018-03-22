7688e4d4dce33e4c5539f752abf46c4c8f766d61
CeliaCha <chazel.celia@gmail.com>
2018-03-22 16:50:51 +0100

    merge routes from the backend

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,36 @@ Route::get('/showPage/{idBD}/{idPage}', function ($idBD, $idPage) {
 }) -> name('showPage');
 
 Route::get('/showPage/{idBD}/{idPage}', 'PageController@show');
+
+
+Route::get('/catalogue', 'ComicsController@show')->name('catalogue');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('/ajouter-bd', function () {
+    return view('ajouter-bd');
+}) -> name('ajouter-bd');
+
+Route::post('ajouter-bd', 'ComicsController@add');
+
+Route::get('/button-update-bd', function(){
+    return view('button-update-bd');
+}) -> name('button-update-bd');
+
+
+Route::get('/update-bd/{id}', 'ComicsController@fetchUniqueBD')->name('update-bd');
+
+
+Route::get('/update-bd', function(){
+    return view('update-bd');
+}) ->name('update-bd');
+Route::post('/update-bd', 'ComicsController@update');
+
+
+Route::get('/delete-bd', function () {
+    return view('delete-bd');
+}) -> name('delete-bd');
+Route::post('/delete-bd', 'ComicsController@delete');
