1bf3f7109057f783ed711b933978053007d70ca8
Kris <christellelocque@hotmail.fr>
2018-03-26 11:21:52 +0200

    finalisation de la page listeMedias, modification d'une route et ajout d'un margin-bottom sur la navbar

--- routes/web.php
+++ routes/web.php
@@ -72,6 +72,7 @@ Route::get('/modifBoard', function () {
     return view('modifBoard');
 })->name('modifBoard');
 
+//frontEnd : visuel de la liste des médias
 Route::get('/listMedias', function () {
     return view('listMedias');
 })->name('listMedias');
