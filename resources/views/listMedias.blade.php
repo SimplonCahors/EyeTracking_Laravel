a4e35a276549fa789b4b19ba4485d6d6dd255829
nico <nicocofer@hotmail.com>
2018-03-26 16:13:25 +0200

    centrage du bouton ajout media

--- resources/views/listMedias.blade.php
+++ resources/views/listMedias.blade.php
@@ -4,27 +4,81 @@
 @endsection
 @section('content')
 <section id="sectionListMedia">
-<h2>Liste des Médias</h2>
 
-<button id="addMedia" class="btn btn-outline-secondary">Ajouter un média</button>
+	<h2>Liste des Médias</h2>
+	<a href="/medias-upload" id="addMedia" class="btn btn-outline-secondary">Ajouter un média</a>
 
-<ul>
-	<li>Type de fichier</li>
-	<li>Nom du fichier</li>
-	<li><img src="" alt="">Miniature</li>
-	<button id="deleteMedia" class="btn btn-outline-secondary">Supprimer</button>
-</ul> 
-<ul>
-	<li>Type de fichier</li>
-	<li>Nom du fichier</li>
-	<li><img src="" alt="">Miniature</li>
-	<button id="deleteMedia" class="btn btn-outline-secondary">Supprimer</button>
-</ul> 
-<ul>
-	<li>Type de fichier</li>
-	<li>Nom du fichier</li>
-	<li><img src="" alt="">Miniature</li>
-	<button id="deleteMedia" class="btn btn-outline-secondary">Supprimer</button>
-</ul> 
+<!-- manque le foreach pour récupérer chaque média dans la BDD. Une seule <div col-s> sera nécessaire dans le foreach -->
+
+	<div class="row justify-content-center">
+
+	  <div class="col-s col-auto">
+		<div class="card" style="width: 18rem;">
+		  <img class="card-img-top" src="/img/plancheBD.JPG" alt="Miniature">
+		  <div class="card-body">
+		    <h5 class="card-title">Nom du fichier</h5>
+		    <p class="card-text">Type de fichier</p>
+		    <a href="#" id="deleteMedia" class="btn btn-primary">Supprimer</a>
+		  </div>
+		</div>
+	  </div>
+
+	    <div class="col-s col-auto">
+		<div class="card" style="width: 18rem;">
+		  <img class="card-img-top" src="/img/plancheBD.JPG" alt="Miniature">
+		  <div class="card-body">
+		    <h5 class="card-title">Nom du fichier</h5>
+		    <p class="card-text">Type de fichier</p>
+		    <a href="#" id="deleteMedia" class="btn btn-primary">Supprimer</a>
+		  </div>
+		</div>
+	  </div>
+
+	    <div class="col-s col-auto">
+		<div class="card" style="width: 18rem;">
+		  <img class="card-img-top" src="/img/plancheBD.JPG" alt="Miniature">
+		  <div class="card-body">
+		    <h5 class="card-title">Nom du fichier</h5>
+		    <p class="card-text">Type de fichier</p>
+		    <a href="#" id="deleteMedia" class="btn btn-primary">Supprimer</a>
+		  </div>
+		</div>
+	  </div>
+
+	    <div class="col-s col-auto">
+		<div class="card" style="width: 18rem;">
+		  <img class="card-img-top" src="/img/plancheBD.JPG" alt="Miniature">
+		  <div class="card-body">
+		    <h5 class="card-title">Nom du fichier</h5>
+		    <p class="card-text">Type de fichier</p>
+		    <a href="#" id="deleteMedia" class="btn btn-primary">Supprimer</a>
+		  </div>
+		</div>
+	  </div>
+
+	    <div class="col-s col-auto">
+		<div class="card" style="width: 18rem;">
+		  <img class="card-img-top" src="/img/plancheBD.JPG" alt="Miniature">
+		  <div class="card-body">
+		    <h5 class="card-title">Nom du fichier</h5>
+		    <p class="card-text">Type de fichier</p>
+		    <a href="#" id="deleteMedia" class="btn btn-primary">Supprimer</a>
+		  </div>
+		</div>
+	  </div>
+
+	    <div class="col-s col-auto">
+		<div class="card" style="width: 18rem;">
+		  <img class="card-img-top" src="/img/plancheBD.JPG" alt="Miniature">
+		  <div class="card-body">
+		    <h5 class="card-title">Nom du fichier</h5>
+		    <p class="card-text">Type de fichier</p>
+		    <a href="#" id="deleteMedia" class="btn btn-primary">Supprimer</a>
+		  </div>
+		</div>
+	  </div>
+
+	</div>
+
 </section>
 @endsection
