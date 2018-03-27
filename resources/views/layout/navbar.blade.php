e40cdabbbbde14ce72074953a3716c6791bc0cae
ElizRe <elizreed@laposte.net>
2018-03-27 14:53:53 +0200

    added menu item of media list for logged in users

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -2,4 +2,7 @@
 	<a href="{{ url('/') }}">Accueil</a>
 	<a href="{{ url('/catalogue') }}">Catalogue</a>
 	<a href="{{ url('/legalmentions') }}">Mentions Légales</a>
+	 @if (Auth::check())
+	<a href="{{ url('/listMedias') }}">Liste Média</a>
+@endif
 </div>
