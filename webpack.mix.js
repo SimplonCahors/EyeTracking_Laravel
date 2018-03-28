84455bd17ecc8a79216354e5ae293a4d08b57dc0
Elisa Demanez <demanez.elisa@gmail.com>
2018-03-28 14:49:41 +0200

    added page_edit.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,5 +13,6 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
     .js('resources/assets/js/libraries/canvasAreaDraw.js', 'public/js')
+    .js('resources/assets/js/page_edit.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
    .browserSync('localhost:8000');
