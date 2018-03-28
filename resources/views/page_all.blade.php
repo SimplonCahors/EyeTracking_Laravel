4b5171dd7ecf39a8b62101f1742a844c4e05e50b
Elisa Demanez <demanez.elisa@gmail.com>
2018-03-28 14:41:26 +0200

    can show mutiples area w/ different styles. waiting for the controller

--- resources/views/page_all.blade.php
+++ resources/views/page_all.blade.php
@@ -3,20 +3,19 @@
    stuff
 @endsection
 @section('content')
-<textarea style="display:none;" rows=3 name="coords1" class="canvas-area input-xxlarge"  
-
-placeholder="Shape Coordinates" 
-
-data-image-url="http://farm8.staticflickr.com/7259/6956772778_2fa755a228.jpg"></textarea>
+<button> Créer zone</button>
 
 
-
+<!-- Quand on aura un controller qui renvoie et les zone(id+coord) et si elles ont un medai de lié, besoin de faire un foreach : if lié ou non -> afficher le area correspondant en remplacant les coords, et l'id par ceux correspondants.  -->
 <img id="background_map"src="http://farm8.staticflickr.com/7259/6956772778_2fa755a228.jpg" alt="Planets" usemap="#planetmap" class="map">
 
  <map id="map_object"name="planetmap">
+<!-- en rouge : avec media -->
+<area id="test_area" shape="poly" coords="208,221,208,202,198,199,201,191,218,176,229,155,221,132,196,117,169,131,157,158,163,172,177,164,173,180,190,185,192,199,187,201,185,222" data-maphilight='{"alwaysOn": true,"strokeColor":"FE2E2E","strokeWidth":2,"fillColor":"FE2E2E","fillOpacity":0.6}' data-style= "without-media" href="" >
 
-<area id="test_area" shape="poly" coords="208,221,208,202,198,199,201,191,218,176,229,155,221,132,196,117,169,131,157,158,163,172,177,164,173,180,190,185,192,199,187,201,185,222" href="#" alt="Sun">
- <area id= "bonjour"shape="poly" coords="160,225,160,250,202,300" href="" alt="Sun">
+<!-- en bleu : sans media -->
+ <area shape="poly"   data-maphilight='{"alwaysOn": true,"strokeColor":"0000ff","strokeWidth":2,"fillColor":"0000ff","fillOpacity":0.6}' data-style= "with-media"coords="160,225,160,250,202,300" href="" >
+ <area shape="poly"   data-maphilight='{"alwaysOn": true,"strokeColor":"0000ff","strokeWidth":2,"fillColor":"0000ff","fillOpacity":0.6}' data-style= "with-media"coords="225,160,250,202,300,160" href="" >
 
 </map>  
 @endsection
@@ -29,23 +28,27 @@ data-image-url="http://farm8.staticflickr.com/7259/6956772778_2fa755a228.jpg"></
 <script>
 // 
 
-//init the map for highlighting 
-        $('.map').maphilight();
+// //init the map for highlighting 
+ $('.map').maphilight();
 
 
-     $(document).ready(function()
-    {
-        var data = {};
-            
-        data.alwaysOn = true;
-        data.fillColor = 'FE2E2E'; 
-        data.strokeColor = '0000FF';
-        data.strokeWidth ='2'
-        data.fillOpacity = '0.5';
-        $('#test_area').data('maphilight', data).trigger('alwaysOn.maphilight');
-        $('#bonjour').data('maphilight', data).trigger('alwaysOn.maphilight');
-        
-    });    
+$(document).ready(function()
+{
+  
+  $('area').each(function() {
+
+    //   console.log($(this), 'ici');
+    var data = $(this).data('maphilight');
+  
+    $(this).data('maphilight', data).trigger('alwaysOn.maphilight');   
+    
+      $(this).click(function(){
+            //    console.log($(this))
+            //    redirige vers ./area/{id}
+            });
+    });
+    
+});  
 </script>
 
 
