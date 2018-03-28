37bce81077cb8a16768b80923028770c327301f5
echayotte <echayotte@gmail.com>
2018-03-28 09:26:10 +0200

    ajajout de commentaire de liaison de fichiers et quelques modifs

--- resources/views/update-bd.blade.php
+++ resources/views/update-bd.blade.php
@@ -18,9 +18,9 @@
             <label for="auteur">Nom de l'auteur</label>
             <input type="text" id="auteur" name="auteur" value="{{$comic->com_author}}"/>
 
-            <label for="miniature">miniature :</label>
+            <label for="miniature">Miniature :</label>
             <input type="file" id="miniature" name="miniature" required />
-            <input type="submit" />
+            <input type="submit" value="MODIFIER"/>
 
         </form>
 
