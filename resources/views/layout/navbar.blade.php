b54288edba8d26c31a361cb7efd1fb50aa0d2e4c
nico <nicocofer@hotmail.com>
2018-03-28 16:29:53 +0200

    correction après pull request , liste des médias ainsi que ajout medias

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,13 +1,9 @@
-
-
 <div id="navbar">
 	<a href="{{ url('/') }}">Accueil</a>
 	<a href="{{ url('/catalogue') }}">Catalogue</a>
 	<a href="{{ url('/legalmentions') }}">Mentions Légales</a>
-
-</div>
-
 	@if (Auth::check())
 	<a href="{{ url('/medias') }}">Médias</a>
 	@endif
 </div>
+
--- resources/views/medias-upload.blade.php
+++ resources/views/medias-upload.blade.php
@@ -6,6 +6,7 @@
 
 @section('content')
 
+<section id="medias-upload">
 
 <!-- si le fichier n'est pas conforme/ne passe pas les validations -->
 @if ($errors->any())
@@ -55,41 +56,6 @@
 <p>son :mpga,wav,ogg,mp4</p></div> -->
 
 
-    <!-- si le fichier n'est pas conforme/ne passe pas les validations -->
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-
-    <!-- si l'envoi dans la db est réusssi. les deux $result possibles sont modifiables dans mediascontroller ligne 45 & 50 -->
-    @php if(isset($result)) echo $result;
-    @endphp
-
-    <form method="post" action="/upload/save" enctype="multipart/form-data" >
-        @csrf
-        <select name="dataType">
-            <option value="img">Image</option> 
-            <option value="son" >Son</option>
-            <option value="video">Video</option>
-        </select>
-        
-        <input type="file" name="file"/>
-        <input type="submit"/>
-    </form>
-
-    <a href="/medias">Retour à la page Medias</a>
-
-    <!--
-        <div>  On croit que : video max 1MB, image max 10MB ?
-            <p> vidéo : mp4,mpeg,quicktime. </p>
-            <p> image : jpeg not working </p>
-            <p>son :mpga,wav,ogg,mp4</p>
-        </div>
-    -->
+</section>
 
 @endsection
