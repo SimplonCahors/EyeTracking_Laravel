71df482e5ce09fa5d3a6285714ecea3f637b877d
Elisa Demanez <demanez.elisa@gmail.com>
2018-03-28 10:56:28 +0200

    added canvasAreaDraw labrary and jquery migrate.

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,6 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
+    .js('resources/assets/js/libraries/canvasAreaDraw.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
    .browserSync('localhost:8000');
