f88f6a577ea31cf71006e227faaaa0dbd89d9efc
echayotte <echayotte@gmail.com>
2018-06-14 13:59:22 +0200

    refacto pour correspondre a R_49_ORM

--- app/Http/Controllers/ComicsControllerRefacto.php
+++ app/Http/Controllers/ComicsControllerRefacto.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use App\Comics;
+use App\Boards;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,41 +36,33 @@ class ComicsController extends Controller
     // [!!] OLD public function "add"(Request $request)
     public function create(Request $request)
     {
+        //store dans le dossier public, le fichier 'miniature'
+        $originalName = $request->file('miniature')->getClientOriginalName();
+        $pathstart = $request->file('miniature')->storeAs('public', $originalName);
+        
+        //enlève le public devant
+        $path = substr($pathstart, 7);
+        
+        $comics = new Comics;
+        $comics-> com_title = request('titre');
+        $comics-> com_author = request('auteur');
+        $comics-> com_publisher = request('editeur');
+
+        $verif_comic = Comics::all()->where('com_title',$comics-> com_title)
+                                    ->where('com_author',$comics-> com_author)
+                                    ->where('com_publisher',$comics-> com_publisher);
+        
+        if(!$verif_comic)
         {
-            //store dans le dossier public, le fichier 'miniature'
-            $originalName = $request->file('miniature')->getClientOriginalName();
-            $pathstart = $request->file('miniature')->storeAs('public', $originalName);
-            
-            //enlève le public devant
-            $path = substr($pathstart, 7);
-    
-            $titre = $request->input('titre');
-            $auteur = $request->input('auteur');
-            $editeur = $request->input('editeur');
-            $comics = DB::table('comics')->get();
-    
-            foreach($comics as $comic)
-            {
-                if( $comic->com_title === $titre && $comic->com_author === $auteur &&$comic->com_publisher === $editeur )
-                {
-                    echo 'Bande déssinée déjà existante';
-                    header('refresh: 3; url = /comics/create');
-                    die;
-                }
-            }
-            
-            DB::table('comics')->insert(
+            $comics->save();
+            return redirect()->route('catalogue')->with('add','BD ajoutée');
+        }
+        else
+        {
+            echo 'Bande déssinée déjà existante';
+            header('refresh: 3; url = /comics/create');
+            die;
 
-               array(
-                   'com_title' => $titre,
-                   'com_author' => $auteur,
-                   'com_publisher' => $editeur,
-                   'com_miniature_url'=> $originalName)
-                );
-
-            return redirect()
-            ->route('catalogue')
-            ->with('add','BD ajoutée');
         }
     }
 
@@ -91,15 +85,12 @@ class ComicsController extends Controller
      */
     public function show()
     {
-        $comics = DB::table('comics')
-        ->where('com_publication', '=', 1)
-        ->get();
+        $comics = Comics::all()->where('com_publication',1);
 
-        $pages = DB::table('pages')
-        ->where('pag_number', '=', 1)
-        ->get();
+        // $comics = DB::table('comics')->where('com_publication', '=', 1)->get();
+        // $pages = DB::table('pages')->where('pag_number', '=', 1)->get();
 
-        return view('catalogue', ['comics' => $comics], ['pages' => $pages]);
+        return view('catalogue', ['comics' => $comics]);
     }
 
     /**
@@ -120,25 +111,19 @@ class ComicsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request)
+    public function update($id, Request $request)
     {
-        $titre = $request->input('titre');
-        $auteur = $request->input('auteur');
-        $editeur = $request->input('editeur');
-        $miniature = $request->input('miniature');
-        $publication = $request->input('radio');
+        $comics = 
+        // $titre = $request->input('titre');
+        // $auteur = $request->input('auteur');
+        // $editeur = $request->input('editeur');
+        // $miniature = $request->input('miniature');
+        // $publication = $request->input('radio');
 
-        DB::table('comics')
-        ->where('com_oid', '=', $id)
-        ->update([
-            'com_title' => $titre, 
-            'com_author' => $auteur, 
-            'com_publisher' => $editeur, 
-            'com_miniature_url' => $miniature, 
-            'com_publication' => $publication
-            ]);
+        DB::table('comics')->where('com_oid', '=', $id)->update(['com_title' => $titre, 'com_author' => $auteur, 'com_publisher' => $editeur, 'com_miniature_url' => $miniature, 'com_publication' => $publication]);
 
-        echo 'la modification a bien été faite';
+
+        echo 'la modif à bien été faite';
         header('refresh: 3; url = '.$id);
     }
 
@@ -155,9 +140,10 @@ class ComicsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
+    
+    // Supprime les miniatures de la DB et du Storage
     public function destroy(Request $request, $id)
     {
-        // Supprime les miniatures de la DB et du Storage
         DB::table('pages')->where('fk_com_oid','=',$id)->delete();
         DB::table('comics')->where('com_oid', '=', $id)->delete();
 
