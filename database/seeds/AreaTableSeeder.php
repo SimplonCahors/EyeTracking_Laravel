e05a24ed92cdec33c81a70562db2a5b517f1a3fd
echayotte <echayotte@gmail.com>
2018-06-15 15:31:48 +0200

    comment ces deux pour l'instant

--- database/seeds/AreaTableSeeder.php
+++ database/seeds/AreaTableSeeder.php
@@ -12,13 +12,12 @@ class AreaTableSeeder extends Seeder
      */
     public function run()
     {
-        
-            $areas = [
-                ['area_id' => 1, 'area_coord' => , 'area_trigger' => , 'fk_board_id' => , 'created_at' => date('Y-m-d H:i:s'),  'updated_at' => date('Y-m-d H:i:s')],
-                ['area_id' => 2, 'area_coord' => , 'area_trigger' => , 'fk_board_id' => , 'created_at' => date('Y-m-d H:i:s'),  'updated_at' => date('Y-m-d H:i:s')],
-                ['area_id' => 3, 'area_coord' => , 'area_trigger' => , 'fk_board_id' => , 'created_at' => date('Y-m-d H:i:s'),  'updated_at' => date('Y-m-d H:i:s')],
-            ];
+        // $areas = [
+        //    ['area_id' => 1, 'area_coord' => , 'area_trigger' => , 'fk_board_id' => , 'created_at' => date('Y-m-d H:i:s'),  'updated_at' => date('Y-m-d H:i:s')],
+        //    ['area_id' => 2, 'area_coord' => , 'area_trigger' => , 'fk_board_id' => , 'created_at' => date('Y-m-d H:i:s'),  'updated_at' => date('Y-m-d H:i:s')],
+        //    ['area_id' => 3, 'area_coord' => , 'area_trigger' => , 'fk_board_id' => , 'created_at' => date('Y-m-d H:i:s'),  'updated_at' => date('Y-m-d H:i:s')],
+        //     ];
 
-        Area::insert($areas);
+        // Area::insert($areas);
     }
 }
--- database/seeds/MediaTableSeeder.php
+++ database/seeds/MediaTableSeeder.php
@@ -14,16 +14,16 @@ class MediaTableSeeder extends Seeder
     public function run()
     {
 
-        $medias = [
-            ['media_id' => 1, 'media_type' => , 'media_filename' => , 'media_path' => , 'fk_area_id' => , 'created_at' => date('Y-m-d H:i:s'),  'updated_at' => date('Y-m-d H:i:s')],
-            ['media_id' => 2, 'media_type' => , 'media_filename' => , 'media_path' => , 'fk_area_id' =>  , 'created_at' => date('Y-m-d H:i:s'),  'updated_at' => date('Y-m-d H:i:s')],
-            ['media_id' => 3, 'media_type' => , 'media_filename' => , 'media_path' => , 'fk_area_id' =>  , 'created_at' => date('Y-m-d H:i:s'),  'updated_at' => date('Y-m-d H:i:s')],
-            ['media_id' => 4, 'media_type' => , 'media_filename' => , 'media_path' => , 'fk_area_id' =>  , 'created_at' => date('Y-m-d H:i:s'),  'updated_at' => date('Y-m-d H:i:s')],
-            ['media_id' => 5, 'media_type' => , 'media_filename' => , 'media_path' => , 'fk_area_id' =>  , 'created_at' => date('Y-m-d H:i:s'),  'updated_at' => date('Y-m-d H:i:s')],
+        // $medias = [
+        //     ['media_id' => 1, 'media_type' => , 'media_filename' => , 'media_path' => , 'fk_area_id' =>  , 'created_at' => date('Y-m-d H:i:s'),  'updated_at' => date('Y-m-d H:i:s')],
+        //     ['media_id' => 2, 'media_type' => , 'media_filename' => , 'media_path' => , 'fk_area_id' =>  , 'created_at' => date('Y-m-d H:i:s'),  'updated_at' => date('Y-m-d H:i:s')],
+        //     ['media_id' => 3, 'media_type' => , 'media_filename' => , 'media_path' => , 'fk_area_id' =>  , 'created_at' => date('Y-m-d H:i:s'),  'updated_at' => date('Y-m-d H:i:s')],
+        //     ['media_id' => 4, 'media_type' => , 'media_filename' => , 'media_path' => , 'fk_area_id' =>  , 'created_at' => date('Y-m-d H:i:s'),  'updated_at' => date('Y-m-d H:i:s')],
+        //     ['media_id' => 5, 'media_type' => , 'media_filename' => , 'media_path' => , 'fk_area_id' =>  , 'created_at' => date('Y-m-d H:i:s'),  'updated_at' => date('Y-m-d H:i:s')],
 
-        ];
+        // ];
 
-        Media::insert($medias);
+        // Media::insert($medias);
 
     }
 }
