d3db328bf76c4c7d98b8ccaa9c07f61a43a35331
Sophiequehen <sophie.quehen@gmail.com>
2018-06-25 12:24:49 +0200

    modification mise en page accueil + navs

--- resources/views/others/home.blade.php
+++ resources/views/others/home.blade.php
@@ -1,61 +1,67 @@
 @extends('layout.app')
 
 @section('title')
-    Accueil
+Accueil
 @endsection
 
 @section('content')
 
-    <h2 class="titres">Reprendre la lecture</h2>
-    <section class="containers_catalog">
+<section class="page-titles">
+    <h2>Reprendre la lecture</h2>
+    <p>/</p>
+</section>
+<section class="containers_catalog">
 
-        <!-- REQUEST TO DISPLAY THE ALREADY READED COMICS ORGANISED BY ASCENDING ORDER -->
-        @foreach ($comics as $comic)
-            <article class="comics_catalog">
+    <!-- REQUEST TO DISPLAY THE ALREADY READED COMICS ORGANISED BY ASCENDING ORDER -->
+    @foreach ($comics as $comic)
+    <article class="comics_catalog">
 
-                <img class="img_catalog" src="{{ $comic->comic_miniature_url }}" alt="cover">
+        <img class="img_catalog" src="{{ $comic->comic_miniature_url }}" alt="cover">
 
-                <div class="infos_catalog">
-                    <ul>
-                        <li>{{$comic->comic_title}}</li>
-                        <li>{{$comic->comic_author}}</li>
-                        <li>{{$comic->comic_publisher}}</li>
-                    </ul>
-                    <div class="read_edit_catalog">
+        <div class="infos_catalog">
+            <ul>
+                <li>{{$comic->comic_title}}</li>
+                <li>{{$comic->comic_author}}</li>
+                <li>{{$comic->comic_publisher}}</li>
+            </ul>
+            <div class="read_edit_catalog">
 
 
-                    <!-- HERE THE IF FOR ADMIN + MODIFY BUTTON -->
-                        <a  href="{{ route ('comics_update', $comic->comic_id ) }} "  id="button_edit_catalog"><button class="btn-catalogue">Modifier</button></a>
-                        <a><button class="btn-catalogue">Lire</button></a>
-                    </div>
-                </div>
+                <!-- HERE THE IF FOR ADMIN + MODIFY BUTTON -->
+                <a  href="{{ route ('comics_update', $comic->comic_id ) }} "  id="button_edit_catalog"><button class="btn-catalogue">Modifier</button></a>
+                <a><button class="btn-catalogue">Lire</button></a>
+            </div>
+        </div>
 
-            </article>
-        @endforeach
-    </section>
+    </article>
+    @endforeach
+</section>
 
-    <h2 class="titres">Dernières BD mises en ligne</h2>
-    <section class="containers_catalog">
-        @foreach ($comics as $comic)
-            <article class="comics_catalog">
+<section class="page-titles">
+    <h2>Dernières BD mises en ligne</h2>
+    <p>/</p>
+</section>
+<section class="containers_catalog">
+    @foreach ($comics as $comic)
+    <article class="comics_catalog">
 
-                <img class="img_catalog" src="{{ $comic->comic_miniature_url }}" alt="cover">
+        <img class="img_catalog" src="{{ $comic->comic_miniature_url }}" alt="cover">
 
-                    <div class="infos_catalog">
-                        <ul>
-                            <li>{{$comic->comic_title}}</li>
-                            <li>{{$comic->comic_author}}</li>
-                            <li>{{$comic->comic_publisher}}</li>
-                        </ul>
-                        <div class="read_edit_catalog">
+        <div class="infos_catalog">
+            <ul>
+                <li>{{$comic->comic_title}}</li>
+                <li>{{$comic->comic_author}}</li>
+                <li>{{$comic->comic_publisher}}</li>
+            </ul>
+            <div class="read_edit_catalog">
 
-                        <!-- HERE THE IF FOR ADMIN + MODIFY BUTTON -->
-                            <a  href="{{ route ('comics_update', $comic->comic_id ) }} "  id="button_edit_catalog"><button class="btn-catalogue">Modifier</button></a>
-                        <a><button class="btn-catalogue">Lire</button></a>
-                        </div>
-                    </div>
+                <!-- HERE THE IF FOR ADMIN + MODIFY BUTTON -->
+                <a  href="{{ route ('comics_update', $comic->comic_id ) }} "  id="button_edit_catalog"><button class="btn-catalogue">Modifier</button></a>
+                <a><button class="btn-catalogue">Lire</button></a>
+            </div>
+        </div>
 
-            </article>
-        @endforeach
-    </section>  
+    </article>
+    @endforeach
+</section>  
 @endsection
